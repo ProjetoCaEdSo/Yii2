936cf00f82ef63e102acb2152d8268957d53694f
carlos <2180687@my.ipleiria.pt>
2019-11-22 15:55:40 +0000

    Carlos mudanças texto basicas

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,18 +7,16 @@ $this->title = 'CES Apartamentos';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Welcome to CES Apartamentos</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <img src="Imagens/Apartamento_pagina_inicial.jpg" width=900/>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Apartamentos Recentes</h2>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
